4ae25b81ab093d9353d4c62ec395f81f2dd7041c
Christos Gkekas <chris.gekas@gmail.com>
2017-12-04 19:16:29 +0000

    Adding support for left and right channel

--- SpectrumAnalyzer/SpectrumAnalyzer.cpp
+++ SpectrumAnalyzer/SpectrumAnalyzer.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+#include "OledDisplay.h"
 
 #define PI 3.14159265359
 
-#define INPUT_PIN A0
+#define LEFT_INPUT_PIN A0
+#define RIGHT_INPUT_PIN A1
 
 #define BUFFER_SIZE 800
 #define SAMPLE_FREQ 76923
 
-int buffer[BUFFER_SIZE];
+int buffer[BUFFER_SIZE] = { 0 };
+double left_dft[COL_COUNT] = { 0.0 };
+double right_dft[COL_COUNT] = { 0.0 };
+OledDisplay display;
 
 // Sets the input pin and initializes the ATmega328 analog to digital converter
 // (ADC). The micro controller's clock speed is 16 MHz and the ADC conversion
@@ -22,15 +27,18 @@ int buffer[BUFFER_SIZE];
 // lower values (8, 4 and 2) are known to cause significant conversion
 // inaccuracies.
 void setup() {
-    pinMode(INPUT_PIN, INPUT);
+    pinMode(LEFT_INPUT_PIN, INPUT);
+    pinMode(RIGHT_INPUT_PIN, INPUT);
+
+    display.init();
 
     ADCSRA = bit(ADEN) | bit(ADPS2);
 }
 
 // Stores samples of the signal attached to the input pin into the buffer.
-void sample() {
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        buffer[i] = analogRead(INPUT_PIN);
+void sample(uint8_t pin) {
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++) {
+        buffer[i] = analogRead(pin);
     }
 }
 
@@ -40,7 +48,7 @@ void sample() {
 double DFT(unsigned int k) {
     double re = 0, im = 0, phase;
 
-    for (int n = 0; n < BUFFER_SIZE; n++) {
+    for (uint16_t n = 1; n < BUFFER_SIZE; n++) {
         phase = 2 * PI * k * n / SAMPLE_FREQ;
         re += buffer[n] * cos(phase);
         im -= buffer[n] * sin(phase);
@@ -49,5 +57,16 @@ double DFT(unsigned int k) {
 }
 
 void loop() {
+    sample(LEFT_INPUT_PIN);
+    for (uint8_t i = 0; i < COL_COUNT; i++) {
+        left_dft[i] = DFT((i + 1) * 100);
+    }
+
+    sample(RIGHT_INPUT_PIN);
+    for (uint8_t i = 0; i < COL_COUNT; i++) {
+        right_dft[i] = DFT((i + 1) * 100);
+    }
+
+    display.write(right_dft, left_dft);
 }
 
