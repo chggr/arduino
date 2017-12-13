70bdc11322433da85ad25d3a1e3fa448a61f3452
Christos Gkekas <chris.gekas@gmail.com>
2017-12-13 21:32:27 +0000

    SpectrumAnalyzer to support displaying samples

--- SpectrumAnalyzer/SpectrumAnalyzer.cpp
+++ SpectrumAnalyzer/SpectrumAnalyzer.cpp
@@ -6,13 +6,18 @@
 #define LEFT_INPUT_PIN A0
 #define RIGHT_INPUT_PIN A1
 
-#define BUFFER_SIZE 800
+#define BUFFER_SIZE 128
 #define SAMPLE_FREQ 76923
 #define FREQ_STEP 1000
 
-int buffer[BUFFER_SIZE] = { 0 };
+int left[BUFFER_SIZE] = { 0 };
+int right[BUFFER_SIZE] = { 0 };
+
 double left_dft[COL_COUNT] = { 0.0 };
 double right_dft[COL_COUNT] = { 0.0 };
+
+bool spectrum = false;
+
 OledDisplay display;
 
 // Sets the input pin and initializes the ATmega328 analog to digital converter
@@ -37,8 +42,8 @@ void setup() {
 }
 
 // Stores samples of the signal attached to the input pin into the buffer.
-void sample(int pin) {
-    for (int i = 0; i < BUFFER_SIZE; i++) {
+void sample(int pin, int* buffer, int size) {
+    for (int i = 0; i < size; i++) {
         buffer[i] = analogRead(pin);
     }
 }
@@ -46,10 +51,10 @@ void sample(int pin) {
 // Calculates the k-th element of the Discrete Fourier transformation of the
 // signal stored in the buffer. This is a complex number, so this method
 // returns its magnitude.
-double DFT(unsigned int k) {
+double DFT(unsigned int k, int* buffer, int size) {
     double re = 0, im = 0, phase;
 
-    for (int n = 1; n < BUFFER_SIZE; n++) {
+    for (int n = 1; n < size; n++) {
         phase = 2 * PI * k * n / SAMPLE_FREQ;
         re += buffer[n] * cos(phase);
         im -= buffer[n] * sin(phase);
@@ -58,16 +63,18 @@ double DFT(unsigned int k) {
 }
 
 void loop() {
-    sample(LEFT_INPUT_PIN);
-    for (int i = 1; i <= COL_COUNT; i++) {
-        left_dft[i] = DFT(i * FREQ_STEP);
-    }
+    // Sample both channels at the same time.
+    sample(LEFT_INPUT_PIN, left, BUFFER_SIZE);
+    sample(RIGHT_INPUT_PIN, right, BUFFER_SIZE);
 
-    sample(RIGHT_INPUT_PIN);
-    for (int i = 1; i <= COL_COUNT; i++) {
-        right_dft[i] = DFT(i * FREQ_STEP);
+    if (spectrum) {
+        for (int i = 1; i <= COL_COUNT; i++) {
+            left_dft[i] = DFT(i * FREQ_STEP, left, BUFFER_SIZE);
+            right_dft[i] = DFT(i * FREQ_STEP, right, BUFFER_SIZE);
+        }
+        display.spectrum(right_dft, left_dft, COL_COUNT);
+    } else {
+        display.samples(left, right, BUFFER_SIZE);
     }
-
-    display.spectrum(right_dft, left_dft, COL_COUNT);
 }
 
