7bb3391dc91d224deff929ea7268ab00df93fa3a
Christos Gkekas <chris.gekas@gmail.com>
2017-12-14 20:08:31 +0000

    Enhance Ecosystem to use EventGenerator

--- Ecosystem/Ecosystem.cpp
+++ Ecosystem/Ecosystem.cpp
@@ -1,16 +1,30 @@
 #include "Arduino.h"
+#include "Event.h"
+#include "EventGenerator.h"
 #include "OledDisplay.h"
 #include "World.h"
 
 World world;
 OledDisplay oled;
+EventGenerator generator;
 
 void setup() {
+    randomSeed(analogRead(0));
+
     oled.init();
+    oled.display(world);
 }
 
 void loop() {
+    delay(random(1000));
+
+    Event* event = generator.generate();
+    world.apply(&event);
+    delete event;
     oled.display(world);
-    delay(10000);
+
+    delay(200);
+    world.cleanup();
+    oled.display(world);
 }
 
